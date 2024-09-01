5d35859a9db4995014a716e2fd5130e2b69c0594
VidyasagarYadav499 <vidyasagaryadav499@gmail.com>
2024-09-01 13:06:43 +0530

    Add source code and project files

---
+++ WEEK - 01/src/SimpleCalc.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+public class SimpleCalc {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("a: ");
+        int a = input.nextInt();
+        System.out.println("b: ");
+        int b = input.nextInt();
+
+        System.out.println("operation (+, -, /, *): ");
+        char op = input.next().charAt(0);
+
+        switch (op) {
+            case '+':
+                System.out.println(a + b);
+                break;
+            case '-':
+                System.out.println(a - b);
+                break;
+            case '/':
+                if (b == 0) {
+                    System.out.println("invalid division");
+                } else {
+                    System.out.println(a / b);
+                }
+                break;
+            case '*':
+                System.out.println(a * b);
+                break;
+            default:
+                System.out.println("not a valid operation");
+        }
+
+    }
+}
---
+++ WEEK - 01/src/sumOfNums.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class sumOfNums {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        
+        System.out.println("sum: " + (a + b));
+    }
+}
