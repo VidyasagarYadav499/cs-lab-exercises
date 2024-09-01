e152cbd757d0e0aef638a677119b4650ea2c0c29
VidyasagarYadav499 <vidyasagaryadav499@gmail.com>
2024-09-01 13:13:42 +0530

    Add source code and project files

---
+++ WEEK - 03/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(Conversion.decimalConversion(1010));
+        System.out.println(Conversion.binaryConversion(10));
+
+    }
+}
